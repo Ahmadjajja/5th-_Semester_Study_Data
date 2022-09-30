91ab94cefafb8e5f597c215bcd9d39ed354282eb
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2022-09-30 08:35:53 +0500

    updated...

---
+++ (CS-509)Digital Signal Processing/L5E1.m
@@ -0,0 +1,33 @@
+%Exercise1
+z = magic(4);
+%a.
+z(3,:)
+%b.
+z(:, 2)
+%c.
+z(2,:)
+%d.
+z(:, 1)
+%e.
+z(3,2)
+%f.
+z(2,2)
+%g.
+z(1,3)
+%h.
+z(3,2)
+%i.
+z(6)
+%j.
+z(4)
+%k.
+z(3)
+%l.
+z(4)
+%m.
+z(4, 1) = 10;   %Z(4, 1) = 10 this qs according to lab but there is error in statement
+%n.
+z(1, 4) = 20;
+
+
+
