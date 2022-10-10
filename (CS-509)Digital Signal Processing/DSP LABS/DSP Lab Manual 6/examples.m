a5442e7c6e4b37712d8837debfb059d47c459539
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2022-10-10 12:11:42 +0500

    web programming css+bootstrap class

---
+++ (CS-509)Digital Signal Processing/DSP LABS/DSP Lab Manual 6/examples.m
@@ -0,0 +1,3 @@
+%Examples
+A = [1 3 5]
+max(A);
---
+++ (CS-509)Digital Signal Processing/Quiz.m
@@ -0,0 +1,23 @@
+%Quiz
+%Question 1
+%a
+X = magic(3);
+
+Transpose = X';
+
+%b
+ power3 = X^3;
+%c
+add = X + 3;
+%d
+Y = [1 4; 2 5; 3 6];
+concatenate = [X , Y];
+
+%e
+%add = X + Y;
+
+%Question 2
+A = zeros(4);
+%Question 3
+character = 'processing';
+character(5);
