a65a8330d37135495ea528660f92070d0131c639
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2022-11-16 18:33:41 +0500

    pointers tricky concept is going on...

--- (CS-507)System Progrmming/C Language work/pointers.c
+++ (CS-507)System Progrmming/C Language work/pointers.c
@@ -31,29 +31,77 @@ int main()
     // // strcpy(str, "test");
     // // strcpy(ptr, "test"); /* does the same as above */
 
+    // Basic Pointer and Array concept clear
+    //  Very important example
 
-    // Pointer and Array concept clear
-    // Very important example
+    // int table[4];
+    // int *ptr;
+    // ptr = table;     // confusion  // stores starting address of array
+    // table[1] = 94;   // confusion
+    // *(ptr + 1) = 94; // confusion
+    // printf("ptr => %d ,,, table[0] => %d ,,,  table => %d",&ptr, &table[0], &table);
+    // printf("\n");
+    // printf("address of starting index of array => %d", table);
+    // printf("\n");
+    // printf("address of starting index of array in ptr => %d", ptr);
+    // printf("\n");
+    // printf("value stores at index 4 in table array => %d", table[1]);
+    // printf("\n");
+    // printf("getting value stores at index 4 in array using ptr => %d", (*(ptr + 1)));
+    // printf("\n");
+    // printf("4th index address => %d", &table[1]);
+    // printf("\n");
+    // printf("4th index address using ptr =>  %d", ((ptr + 1)));
+
+    // Below code is Very Tricky
+
+    // char str[] = "Test";
+    // char *p;
+    // int i;
+    // for (p = str, i = 0; *p != '\0'; p++, i++)
+    // printf("\n The length of the string is %d and value at this index is %d ", i,*p);
+
+    // // printf("\n%d", ('\0' == 0));   // output 1 means condition is true
+
+    // NULL pointer
 
 
-    int table[4];
-    int *ptr;
-    ptr = table;     // confusion  // stores starting address of array
-    table[1] = 94;   // confusion
-    *(ptr + 1) = 94; // confusion
-    printf("ptr => %d ,,, table[0] => %d ,,,  table => %d",&ptr, &table[0], &table);
-    printf("\n");
-    printf("address of starting index of array => %d", table);
-    printf("\n");
-    printf("address of starting index of array in ptr => %d", ptr);
-    printf("\n");
-    printf("value stores at index 4 in table array => %d", table[1]);
-    printf("\n");
-    printf("getting value stores at index 4 in array using ptr => %d", (*(ptr + 1)));
-    printf("\n");
-    printf("4th index address => %d", &table[1]);
-    printf("\n");
-    printf("4th index address using ptr =>  %d", ((ptr + 1)));
+
+    int *yPtr, y = 5;
+    yPtr = &y;
+
+    // printf("\n *yPtr => %d",*yPtr);
+    // printf("\n y => %d",y);
+   
+    // *yPtr = 7;
+
+    // printf("\n *yPtr => %d",*yPtr);
+    printf("\n y => %d",*&y);
+
+
+
 
     return 0; /* indicate that program ended successfully */
 }
+
+
+// //Very Tricky and Interesting Example
+
+// void MyPrint(char *txt);
+// void main()
+// {
+//     char *msg = NULL;
+//     char x = 'A';
+//     msg = &x;
+//     printf("%c",msg);   // prints address
+//     printf("\n%c",*msg);  // prints actual value
+
+//     MyPrint(msg);
+// }
+// void MyPrint(char *txt)
+// {
+//     if (txt == NULL)
+//         printf("Invalid parameters: NULL pointer received\n");
+//     else
+//         printf("\n%c\n", *txt);   // prints actual value
+// }
